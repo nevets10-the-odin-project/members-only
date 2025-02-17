11ace5d9ebf0e008649b09caa2b7f05fe14c0ba0
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-16 19:36:20 -0800

    Add #new and #create to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.expect(user: %i[email username password])
+  end
 end
